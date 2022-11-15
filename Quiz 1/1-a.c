7ab6545243d6e114ffebe6b6b38b6af16656c603
AatreyiDutt <aatreyi.dutt_ug23@ashoka.edu.in>
2022-11-15 13:07:14 +0530

    improved fizzbuzz

--- Quiz 1/1-a.c
+++ Quiz 1/1-a.c
@@ -7,13 +7,16 @@ void basicFizzBuzz(int x){
   //3,5 Fizz
   //11, 13 buzz
   //33,39,55,65 Fizzbuzz
-  if (x%33==0 || x%39==0 || x%55==0 || x%65==0){
-    printf("fizzbuzz\n");
-  } else if (x%11==0 || x%13==0){
-    printf("buzz\n");
-  } else if (x%3==0 || x%5==0){
-    printf("fizz\n");
-  } else {
+  int flag = 0;
+  if (x%3==0 || x%5==0){
+    printf("fizz");
+    flag++;
+  }
+  if (x%11==0 || x%13==0){
+    printf("buzz");
+    flag++;
+  }
+  if (flag == 0){
     printf("%d\n", x);
   }
 }
