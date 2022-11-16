a12771ed333898e7714e1a6b353781427da0dfd9
AatreyiDutt <aatreyi.dutt_ug23@ashoka.edu.in>
2022-11-17 00:23:34 +0530

    size test

--- insertionSortArray.c
+++ insertionSortArray.c
@@ -1,27 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int* createArray(){
-  static int arr[10];
-  int i;
-  for (i=0; i<10; i++){
-    arr[i] = rand()%100; // insert a random number from 0 to 99
-  }
-  // print Array
+int* insertionSort(int * arr, int size){
+  int i, item, j;
+    for (i = 1; i < size; i++) {
+        item = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > item) {
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = item;
+    }
   return arr;
 }
 
-int* insertionSort(int arr[]){
-  
-  return arrs;
-}
-
-
-void main(){
-  int a;
-  int *arr = createArray();
-  for (a=0; a<10; a++){
+void traverseList(int* arr){
+  for (int a=0; a<10; a++){
     printf("%d ", arr[a]);
   }
-  arrs = insertionSort(arr);
+  printf("\n");
+}
+
+void main(){
+  static int arr[10]; // array creation
+  for (int i=0; i<10; i++){
+    arr[i] = rand()%100; // insert a random number from 0 to 99
+  }
+  size_t s = sizeof(arr)/sizeof(arr[0]);
+  traverseList(arr);
+  int *arrs = insertionSort(arr, s);
+  traverseList(arrs);
 }
