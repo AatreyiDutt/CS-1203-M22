93fed445e647af8112ae1576ec8b75494108d616
AatreyiDutt <aatreyi.dutt_ug23@ashoka.edu.in>
2023-03-20 23:42:57 +0530

    final

--- stressTest.c
+++ stressTest.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void main(){
+    double x = 10;
+    int iter = 26;
+    for (int i=1; i<iter; i++){
+        if(i==23)printf("double starts messing up here:\n");    // based on observation
+        printf("10^%d = %f\n", i, x);
+        x = x*10;
+    }
+}
+
 
